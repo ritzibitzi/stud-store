41f670a618433fb4e153ea32618da64c5f83016e
mpadil18 <mpadil18@ucsc.edu>
2021-06-19 00:44:55 -0700

    Final

--- student-store-express-api/server.js
+++ student-store-express-api/server.js
@@ -5,14 +5,13 @@ const lowdb = require("lowdb");
 const storeRouter = require("./routes/store")
 
 const app = express();
+
 app.use(morgan("tiny"));
+app.use(express.json());
+app.use(cors());
 app.use("/store", storeRouter);
 
-const port = 3000;
-
-app.get("/", (req, res, next) => {
-    res.status(200).json({ping:"pong"});
-});
+const port = 3001;
 
 app.listen(port, () => {
     console.log("Server is running on port", {port});
